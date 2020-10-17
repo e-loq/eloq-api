aa0eb32454a7f172051648a173b2655896ee17ae
Manuel Lang <manuellang183@gmail.com>
2020-10-17 16:27:52 +0200

    api to load url

--- api.py
+++ api.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import sys
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/upload', methods=["POST"])
+def upload_file():
+    print("asdf")
+    data = request.get_json()
+    url = data["url"]
+    return jsonify(data) 
+
+if __name__ == "__main__":
+    app.run(debug=True)
