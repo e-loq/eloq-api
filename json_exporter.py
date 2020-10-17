d83f57e26d1e54d1dc18d15a6e88337749960afd
Manuel Lang <manuellang183@gmail.com>
2020-10-17 13:55:21 +0200

    easy exporter

--- json_exporter.py
+++ json_exporter.py
@@ -1,24 +1,28 @@
 import json
 
-def to_json():
+def export(output_path: str, points: dict):
+    """
+    Exports the data to the desired JSON schema
+
+    Args:
+        output_path (str): destination file path
+        points (dict): point data according to the specific schema
+    """
     ceiling_height = 0
     max_sat_height = 0
-    layers = []
     data = {
         "unit": "m",
         "z_ceil":  ceiling_height,
         "z_sat": max_sat_height,
         "z_marker": 1,  #  NOTE this is given by TRUMPF
-        "layers": layers,
+        "layers": points,
         "optimize": True,
         "marker_grid": 1,
         "sat_grid": 10
     }
 
-    return data
+    with open(output_path, 'w') as f:
+        json.dump(data, f)
 
 if __name__ == "__main__":
-    json_data = to_json()
-    with open('data/output.json', 'w') as f:
-        json.dump(json_data, f)
-    print(json_data)
+    export('data/output.json', {})
